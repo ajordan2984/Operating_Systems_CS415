2c22d6e6662fb180fce10ffc2ddc40b526af6293
ajordan2984 <ajordan2984@yahoo.com>
2017-02-12 16:13:03 -0600

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <array>
 #include <cmath>
 #include <algorithm>
 #include <chrono>
 #include <thread>
 #include<fstream>
 using namespace std;
-
-// Functions provided by Dr.Lewis
 class Timer{
 public:
 	Timer() : beg_(clock_::now()) {}
@@ -27,16 +27,22 @@ void mySleep(unsigned long timeInSeconds)
 }
 void doSomething() { mySleep(3); };
 void doSomeMoreWork() { mySleep(4); };
-// My functions
-double test(int);
+//[row][col]
 void printM(int **, int);
+double test(int);
 void fill(int **arry, int input, int resized);
 int pow2(int data);
-void mulM(int*,int*,int*,int **A, int **B, int **Accum, int max);
-void addM(int*, int*,int*, int **A, int **B, int **Accum, int max);
-void subM(int *, int *,int *, int **A, int **B, int **Accum, int max);
+void addM(int **A, int**B, int **C, int max);
+void addM(int *Adim, int *Bdim, int **A, int **B, int **C, int max);
+void addM(int **A, int **B, int **C, int *Cdim, int max);
+
+void subM(int **A, int **B, int **C, int max);
+void subM(int *Adim, int *Bdim, int **A, int **B, int **C, int max);
+void subM(int **A, int **B, int **C, int *Cdim, int max);
+
+void mulM(int **sub1, int **sub2, int **Accum, int max);
 int** createM(int msize);
-void split(int *, int *,int *, int **A, int **B, int **joinedC, int max);
+void split(int **A, int**B, int **C, int *Cdim, int max);
 void deleteM(int** data, int S);
 void deleteB(int*);
 int main()
@@ -50,15 +56,15 @@ int main()
 	}
 	else{
 		out << "Data Sets| Time for Matrix Multiplication" << endl;
-		out << "100:     " << test(5) << endl;
-		//cout << "100 Finished." << endl;
-		//out << "500:     " << test(500) << endl;
-		//cout << "500 Finished." << endl;
-		//out << "1000:    " << test(1000) << endl;
-		//cout << "1000 Finished." << endl;
+		out << "100:     " << test(100) << endl;
+		cout << "100 Finished." << endl;
+		out << "500:     " << test(500) << endl;
+		cout << "500 Finished." << endl;
+		out << "1000:    " << test(1000) << endl;
+		cout << "1000 Finished." << endl;
 		//out << "5000:    " << test(5000) << endl;
 		//cout << "5000 Finished." << endl;
-		//out << "10000:   " << test(10000) << endl;
+		//out << "10000:   " << test(10000) << endl; */
 	}
 	cout << "Finished execution and writing file." << endl;
 	return 0;
@@ -69,33 +75,25 @@ double test(int input)
 	Timer tmr;
 	// 1st matrix
 	int **test1 = createM(resized);
-	fill(test1, input-1, resized);
+	fill(test1, input - 1, resized);
 	// 2nd matrix
 	int **test2 = createM(resized);
-	fill(test2, input-1, resized);
+	fill(test2, input - 1, resized);
 	// Resulting matrix
 	int **result = createM(resized);
-
-	int *test1_bounds = new int[2]{0, 0};
-	int *test2_bounds = new int[2]{0, 0};
-	int *result_bounds = new int[2]{0, 0};
-
-	printM(test1, resized);
-	printM(test2, resized);
+	int *resultC = new int[2]{0, 0};
 	// Reset timer
 	tmr.reset();
 	doSomething();
 	// Start MM
-	split(test1_bounds, test2_bounds, result_bounds, test1, test2, result, resized);
+	split(test1, test2, result,resultC, resized);
 	// find endtime
 	doSomeMoreWork();
 	double t = tmr.elapsed();
 
-	printM(result, resized);
+	//printM(result, resized);
 	// clean up
-	deleteB(test1_bounds);
-	deleteB(test2_bounds);
-	deleteB(result_bounds);
+	deleteB(resultC);
 	deleteM(result, resized);
 	deleteM(test1, resized);
 	deleteM(test2, resized);
@@ -117,10 +115,10 @@ void fill(int **arry, int input, int resized)
 	for (int i = 0; i < resized; i++)
 		for (int j = 0; j < resized; j++)
 		{
-			if (i > input || j>input)
-				arry[i][j] = 0;
-			else
-				arry[i][j] = 2;
+		if (i > input || j>input)
+			arry[i][j] = 0;
+		else
+			arry[i][j] = 2;
 		}
 }
 int pow2(int data)
@@ -130,51 +128,64 @@ int pow2(int data)
 		i++;
 	return pow(2, i);
 }
-void mulM(int *Abounds, int *Bbounds,int *AccumB, int **A, int **B, int **Accum, int max)
+void addM(int **A, int **B, int **C, int max)
 {
-	// Abounds[row start][col start]
-	// Bbounds[row start][col start]
-	// Accumulator bounds[row start][col start]
-	int Arow = Abounds[0];
-	int Acol = Abounds[1];
-	int Brow = Bbounds[0];
-	int Bcol = Bbounds[1];
-	int ACrow = AccumB[0];
-	int ACcol = AccumB[1];
-	
-	Accum[ACrow][ACcol] = A[Arow][Acol] * B[Brow][Bcol];
+	for (int i = 0; i < max; i++)
+		for (int j = 0; j < max; j++)
+			C[i][j] = A[i][j] + B[i][j];
 }
-void addM(int *Abounds, int *Bbounds, int *AccumB, int **A, int **B, int **Accum, int max)
+void addM(int *Adim, int *Bdim, int **A, int **B, int **C, int max)
 {
 	// Abounds[row start][col start]
 	// Bbounds[row start][col start]
 	// Accumulator bounds[row start][col start]
-	int Arow = Abounds[0];
-	int Acol = Abounds[1];
-	int Brow = Bbounds[0];
-	int Bcol = Bbounds[1];
-	int ACrow = AccumB[0];
-	int ACcol = AccumB[1];
+	int Arow = Adim[0];
+	int Acol = Adim[1];
+	int Brow = Bdim[0];
+	int Bcol = Bdim[1];
 
 	for (int i = 0; i < max; i++)
 		for (int j = 0; j < max; j++)
-			Accum[i + ACrow][j + ACcol] = A[i + Arow][j + Acol] + B[i + Brow][j + Bcol];
+			C[i][j] = A[i + Arow][j + Acol] + B[i + Brow][j + Bcol];
 }
-void subM(int *Abounds, int *Bbounds, int *AccumB, int **A, int **B, int **Accum, int max)
+void addM(int **A, int **B, int **C,int *Cdim, int max)
 {
-	// Abounds[row start][col start]
-	// Bbounds[row start][col start]
-	// Accumulator bounds[row start][col start]
-	int Arow = Abounds[0];
-	int Acol = Abounds[1];
-	int Brow = Bbounds[0];
-	int Bcol = Bbounds[1];
-	int ACrow = AccumB[0];
-	int ACcol = AccumB[1];
-
+	int Crow = Cdim[0];
+	int Ccol = Cdim[1];
 	for (int i = 0; i < max; i++)
 		for (int j = 0; j < max; j++)
-			Accum[i + ACrow][j + ACcol] = A[i + Arow][j + Acol] - B[i + Brow][j + Bcol];
+			C[i+Crow][j+Ccol] = A[i][j] + B[i][j];
+}
+void subM(int **A, int **B, int**C, int max)
+{
+	for (int i = 0; i < max; i++)
+		for (int j = 0; j < max; j++)
+			C[i][j] = A[i][j] - B[i][j];
+}
+void subM(int *Adim, int *Bdim, int **A, int **B, int **C, int max)
+{
+	int Arow = Adim[0];
+	int Acol = Adim[1];
+	int Brow = Bdim[0];
+	int Bcol = Bdim[1];
+	for (int i = 0; i < max; i++)
+		for (int j = 0; j < max; j++)
+			C[i][j] = A[i + Arow][j + Acol] - B[i + Brow][j + Bcol];
+}
+void subM(int **A, int **B, int **C, int *Cdim, int max)
+{
+	int Crow = Cdim[0];
+	int Ccol = Cdim[1];
+	for (int i = 0; i < max; i++)
+		for (int j = 0; j < max; j++)
+			C[i+Crow][j+Ccol] = A[i][j] - B[i][j];
+}
+void mulM(int **sub1, int **sub2, int **Accum, int max)
+{
+	for (int i = 0; i < max; i++)
+		for (int k = 0; k < max; k++)
+			for (int j = 0; j < max; j++)
+				Accum[i][j] += (sub1[i][k] * sub2[k][j]);
 }
 int** createM(int msize)
 {
@@ -187,92 +198,109 @@ int** createM(int msize)
 	}
 	return temp;
 }
-void split(int *Astart, int *Bstart, int *Cstart, int **A, int **B, int **joinedC, int max)
+void split(int **A, int **B, int **C,int *Cdim, int max)
 {
-	if ((max / 2) < 1)
-		mulM(Astart,Bstart,Cstart,A, B, joinedC, max);
-
+	if ((max / 2) < 2)
+		mulM(A, B, C, max);
 	else
 	{
 		int half = max / 2;
-		int Arow = Astart[0];
-		int Acol = Astart[1];
-		int Brow = Bstart[0];
-		int Bcol = Bstart[1];
+		int **sub1 = createM(half);
+		int **sub2 = createM(half);
+		int *Zero = new int[2]{0, 0};
 		
+		int **A11 = createM(half);
+		int *A12_corner = new int[2]{0, half};
+		int *A21_corner = new int[2]{half, 0};
+		int **A22 = createM(half);
 		
-		// Where corners of A start
-		int *A11 = new int[2]{Arow, Acol};
-		int *A12 = new int[2]{ Arow,half};
-		int *A21 = new int[2]{ half,Acol};
-		int *A22 = new int[2]{ half,half};
-		// Where corners of B start
-		int *B11 = new int[2]{Brow,Bcol};
-		int *B12 = new int[2]{ Brow,half };
-		int *B21 = new int[2]{ half,Bcol};
-		int *B22 = new int[2]{ half,half };
-		// Temp matrix to store inbetween answers
-		int **temp1 = createM(half);
-		int **temp2 = createM(half);
-		// Where M1 - M7 row&column matricies start
-		int *Mbounds = new int[2]{0, 0};
-		int *tempb = new int[2]{0, 0};
-		
+		int **B11 = createM(half);
+		int *B12_corner = new int[2]{0,half};
+		int *B21_corner = new int[2]{half,0};
+		int **B22 = createM(half);
+
+		//[row][column]
+
+		for (int i = 0; i < half; i++)
+			for (int j = 0; j < half; j++)
+			{
+				A11[i][j] = A[i][j];
+				A22[i][j] = A[i + half][j + half];
+				B11[i][j] = B[i][j];
+				B22[i][j] = B[i + half][j + half];
+			}//end for spliting matrix
+
 		//M1
 		int **M1 = createM(half);
-		addM(A11, A22,tempb, A,A,temp1,half);
-		addM(B11, B22,tempb, B,B,temp2, half);
-		split(tempb, tempb, Mbounds,temp1, temp2, M1, half);
-
+		addM(A11, A22, sub1, half);
+		addM(B11, B22, sub2, half);
+		split(sub1, sub2, M1,Zero, half);
+		
 		//M2
 		int **M2 = createM(half);
-		addM(A21, A22,tempb,A,A,temp1,half);
-		split(B11,tempb,Mbounds, B, temp1, M2, half);
+		addM(A21_corner,Zero,A, A22, sub1, half);
+		split(sub1, B11, M2,Zero, half);
 
 		//M3
 		int **M3 = createM(half);
-		subM(B12, B22,tempb,B,B,temp1, half);
-		split(A11,tempb,Mbounds, A, temp1, M3, half);
+		subM(B12_corner,Zero,B, B22, sub1, half);
+		split(A11, sub1, M3,Zero, half);
 
 		//M4
 		int **M4 = createM(half);
-		subM(B21, B11,tempb,B, B,temp1,half);
-		split(A22,tempb,Mbounds, A, temp1, M4, half);
-	
+		subM(B21_corner,Zero,B,B11, sub1, half);
+		split(A22, sub1, M4,Zero, half);
+
 		//M5
 		int **M5 = createM(half);
-		addM(A11, A12,tempb, A,A,temp1,half);
-		split(B22,tempb,Mbounds, B, temp1, M5, half);
+		addM(A12_corner,Zero,A,A11, sub1, half);
+		split(sub1, B22, M5,Zero, half);
 
 		//M6
 		int **M6 = createM(half);
-		subM(A21, A11,tempb, A,A,temp1,half);
-		addM(B11, B12,tempb, B,B,temp2,half);
-		split(tempb,tempb,Mbounds, temp1, temp2, M6, half);
+		subM(A21_corner,Zero,A, A11, sub1, half);
+		addM(B12_corner,Zero,B,B11, sub2, half);
+		split(sub1, sub2, M6,Zero, half);
 
 		//M7
 		int **M7 = createM(half);
-		subM(A12, A22,tempb, A,A,temp1, half);
-		addM(B21, B22,tempb, B,B,temp2, half);
-		split(tempb, tempb,Mbounds,temp1, temp2, M7, half);
+		subM(A12_corner,Zero,A, A22, sub1, half);
+		addM(B21_corner,Zero,B, B22, sub2, half);
+		split(sub1, sub2, M7,Zero,half);
 
-		int Crow = Cstart[0];
-		int Ccol = Cstart[1];
-		int *C11 = new int[2]{ Crow,Ccol};
-		int *C12 = new int[2]{ Crow,half };
-		int *C21 = new int[2]{ half,Ccol };
-		int *C22 = new int[2]{ half,half };
+		// Clean up
+		deleteM(A11, half);
+		deleteM(A22, half);
+		deleteM(B11, half);
+		deleteM(B22, half);
+		deleteB(A12_corner);
+		deleteB(A21_corner);
+		deleteB(B12_corner);
+		deleteB(B21_corner);
+		//
+		int crow = Cdim[0];
+		int ccol = Cdim[1];
 		
-		addM(Mbounds, Mbounds,tempb, M1, M7, temp1, half);// M1 + M7 
-		subM(Mbounds, Mbounds,tempb, M4, M5, temp2, half);// M4 - M5
-		addM(tempb,tempb, C11, temp1, temp2, joinedC, half);// C11
-		addM(Mbounds, Mbounds, C12, M3, M5, joinedC, half);// C12 (M3 + M5)
-		addM(Mbounds, Mbounds, C21, M2, M4, joinedC, half);// C21 (M2 + M4)
-		subM(Mbounds, Mbounds,tempb, M1, M2, temp1, half);// M1 - M2
-		addM(Mbounds, Mbounds,tempb, M3, M6, temp2, half);// M3 + M6
-		addM(tempb, tempb, C22, temp1, temp2, joinedC, half);// C22 (M1 - M2 + M3 + M6)
+		int *C11 = new int[2]{crow, ccol};
+		int *C12 = new int[2]{crow, half};
+		int *C21 = new int[2]{half, ccol};
+		int *C22 = new int[2]{half, half};
 		
-		// Delete used matrix
+		addM(M1, M7, sub1, half);
+		subM(M4, M5, sub2, half);
+		// C11
+		addM(sub1, sub2,C, C11, half);
+		//C12
+		addM(M3, M5,C, C12, half);
+		//C21
+		addM(M2, M4,C, C21, half);
+		//C22
+		subM(M1, M2, sub1, half);
+		addM(M3, M6, sub2, half);
+		//M1 - M2 + M3 + M6
+		addM(sub1, sub2, C,C22, half);
+
+		// Delete temp matricies
 		deleteM(M1, half);
 		deleteM(M2, half);
 		deleteM(M3, half);
@@ -280,16 +308,9 @@ void split(int *Astart, int *Bstart, int *Cstart, int **A, int **B, int **joined
 		deleteM(M5, half);
 		deleteM(M6, half);
 		deleteM(M7, half);
-		// Delete bounds
-		deleteB(A11);
-		deleteB(A12);
-		deleteB(A21);
-		deleteB(A22);
-		deleteB(B11);
-		deleteB(B12);
-		deleteB(B21);
-		deleteB(B22);
-		deleteB(Mbounds);
+		deleteM(sub1, half);
+		deleteM(sub2, half);
+		// Delete Corners
 		deleteB(C11);
 		deleteB(C12);
 		deleteB(C21);
@@ -304,8 +325,8 @@ void deleteM(int** data, int S)
 	delete[] data;
 	data = nullptr;
 }
-void deleteB(int* data)
+void deleteB(int*data)
 {
-	delete [] data;
+	delete[] data;
 	data = nullptr;
 }
